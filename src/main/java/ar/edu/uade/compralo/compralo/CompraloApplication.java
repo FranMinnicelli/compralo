e3db3d836ddb35d0d2e5b2515e4dd392b0d61d1f
Matias Felau <mfelau@uade.edu.ar>
2025-10-02 20:38:06 -0300

    Initial commit

---
+++ src/main/java/ar/edu/uade/compralo/compralo/CompraloApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.compralo.compralo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/uade/compralo/compralo/CompraloApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.compralo.compralo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
