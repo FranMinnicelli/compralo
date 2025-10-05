6370053eb51c8072996570e4191441a37db2d5c5
minnicef <minnicef@gmail.com>
2025-10-05 15:25:10 -0300

    Greedy para recomendaciones

--- src/main/java/ar/edu/uade/compralo/compralo/service/RecomendacionService.java
+++ src/main/java/ar/edu/uade/compralo/compralo/service/RecomendacionService.java
@@ -5,6 +5,8 @@ import ar.edu.uade.compralo.compralo.utils.DijkstraUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -20,7 +22,26 @@ public class RecomendacionService {
         Map<Producto, Double> distancias = DijkstraUtils.calcularCaminos(productos, producto);
 
         //algoritmo greedy
-
-        return Set.of();
+        //Lo vamos a buscar por distancia, el producto con mas probabilidad de ser comprado es el mas cercano, entonces greedy
+        //va a recibir el map de distnacias que esta arriba y una cantidad de recomendaciones n (cuantas le paso al front, 1,2,3,4)
+        //Esto me va a devolver un set con los n productos mas cercanos, seria del tipo de dato listado de productos
+        //Entonces, recibe "distancias" y n y retorna productos mas cercanos
+        return obtenerRecomendacionesGreedy(distancias, n);
     }
+
+    public Set<Producto> obtenerRecomendacionesGreedy(Map<Producto, Double> distancias, int n){
+        Set<Producto> recomendaciones = new HashSet<>();
+        Double distanciaMenor = Double.POSITIVE_INFINITY;
+        for(Producto p : distancias.keySet()){
+            if(recomendaciones.size() == n) break;
+            if(distancias.get(p) < distanciaMenor){
+                distanciaMenor = distancias.get(p);
+                recomendaciones.add(p);
+            }
+        }   
+        return recomendaciones;
+    }
+
+
+    
 }
